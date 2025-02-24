48046ef45de4d05c657d5e8be91752b967924e5f
Daniel <dtal@attentigroup.com>
2025-02-24 09:16:13 +0200

    Section2: Common Library - Database & Config Module

---
+++ sleepr/libs/common/src/database/database.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '../config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+    imports: [MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGODB_URI'),
+        }),
+        inject: [ConfigService],
+    })]
+})
+export class DatabaseModule {}
